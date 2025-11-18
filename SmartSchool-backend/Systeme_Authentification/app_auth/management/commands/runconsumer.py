68606a7f95dc3f26a635c346d81d520af84da49c
LaVue <sandjonyves@gmail.com>
2025-11-18 20:48:39 +0100

    feat. loraqu'on enreigistre un utilisateur on creer directement son compte d;authentification

--- SmartSchool-backend/Systeme_Authentification/app_auth/management/commands/runconsumer.py
+++ SmartSchool-backend/Systeme_Authentification/app_auth/management/commands/runconsumer.py
@@ -1,12 +1,19 @@
-# management/commands/runconsumer.py
 from django.core.management.base import BaseCommand
-from app_auth.consumers.rabbitmq_consumer import RabbitMQConsumer
+from app_auth.consumers.rabbitmq_consumer import RabbitMQConsumer, RabbitMQRegistrationConsumer
 
 class Command(BaseCommand):
-    help = 'Lance le consumer RabbitMQ'
+    help = 'Lance les consumers RabbitMQ'
 
     def handle(self, *args, **options):
-        consumer = RabbitMQConsumer()
-        consumer.start()
-        self.stdout.write(self.style.SUCCESS('Consumer RabbitMQ démarré'))
-        consumer.join()
+        auth_consumer = RabbitMQConsumer()
+        registration_consumer = RabbitMQRegistrationConsumer()
+
+        # Lancer en parallèle
+        auth_consumer.start()
+        registration_consumer.start()
+
+        self.stdout.write(self.style.SUCCESS('🔥 Consumers RabbitMQ démarrés (auth + registration)'))
+
+        # Attendre les threads (facultatif mais recommandé)
+        auth_consumer.join()
+        registration_consumer.join()
