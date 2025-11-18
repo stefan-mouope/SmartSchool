7f2b72118013d0f14029973dcff412613086a56a
LaVue <sandjonyves@gmail.com>
2025-11-18 21:12:24 +0100

    merge stefan

--- registration-service/src/modules/registration/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as registrationRoutes } from "./registration.module";
-export * from "./school/school.model";
-export * from "./teacher/teacher.model";
-export * from "./director/director.model";
-export * from "./classroom/classroom.model";
-export * from "./matter/matter.model";
-export * from "./academicYear/academicYear.model";
-
-
